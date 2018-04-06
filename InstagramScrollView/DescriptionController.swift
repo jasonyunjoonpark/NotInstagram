f07a32da5487622c9f4fd4778e4b5e6e2540b14f
Jason Park <34497076+yunjoonpark@users.noreply.github.com>
2018-04-06 18:35:00 -0400

    Refactor: HomeController + DescriptionController.

--- InstagramScrollView/DescriptionController.swift
+++ InstagramScrollView/DescriptionController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class DescriptionController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func shareButtonPressed(_ sender: Any) {
+        let storageRef = Storage.storage().reference()
+//        let uploadData = UIImagePNGRepresentation(selectedImage!)
+        
+        let fileName = UUID().uuidString
+        
+//        storageRef.child(fileName).putData(uploadData!, metadata: nil, completion: { (metadata, error) in
+//            if error != nil {
+//                print(error)
+//                return
+//            }
+//            
+//        })
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+        
+        imageView.image = UIImage(named: "")
         }
     
 }
--- InstagramScrollView/HomeController+handlers.swift
+++ InstagramScrollView/HomeController+handlers.swift
@@ -40,32 +40,18 @@ extension HomeController: UIImagePickerControllerDelegate, UINavigationControlle
         } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
             selectedImage = originalImage
         }
-        
         picker.dismiss(animated: true, completion: nil)
-
         myGroup.leave()
         
         
         //Store user selected image into Firebase Storage after completion of storing user selected image into variable: selectedImage
         myGroup.notify(queue: .main) {
-            
+        
+            //Present Description Controller
             let descriptionVC: DescriptionController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "descriptionController") as UIViewController as! DescriptionController
             
             self.present(descriptionVC, animated: true, completion: nil)
-
             
-            let storageRef = Storage.storage().reference()
-            let uploadData = UIImagePNGRepresentation(selectedImage!)
-            
-            let fileName = UUID().uuidString
-            
-            storageRef.child(fileName).putData(uploadData!, metadata: nil, completion: { (metadata, error) in
-                if error != nil {
-                    print(error)
-                    return
-                }
-                
-            })
             
         }
 
