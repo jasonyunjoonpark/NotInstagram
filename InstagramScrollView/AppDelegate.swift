be0080adcb7780c7e2c234ea477f27b081b21b6a
Jason Park <34497076+yunjoonpark@users.noreply.github.com>
2018-04-10 20:34:38 -0400

    Update: fix double posts fetch from Firebase.

--- InstagramScrollView/AppDelegate.swift
+++ InstagramScrollView/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
+        //Anonymous Firebase Sign-In
+        Auth.auth().signInAnonymously() { (user, error) in
+            
+        }
         return true
     }
 
--- InstagramScrollView/HomeController.swift
+++ InstagramScrollView/HomeController.swift
@@ -10,7 +10,11 @@ import UIKit
 import Firebase
 
 class HomeController: UIViewController {
-
+    
+    var ref: DatabaseReference?
+    var refHandle: UInt!
+    var posts = [Post]()
+    
     //MARK: IBOutlets
     @IBOutlet weak var tableView: UITableView!
     
@@ -18,9 +22,7 @@ class HomeController: UIViewController {
     @IBAction func uploadImageButtonPressed(_ sender: Any) {
         perform(#selector(handleUploadImageButtonPressed))
     }
-    
-    var posts = [Post]()
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
@@ -32,57 +34,86 @@ class HomeController: UIViewController {
         //Delegates
         tableView.delegate = self
         tableView.dataSource = self
+
+        if let uid = Auth.auth().currentUser?.uid {
+            ref = Database.database().reference()
+        }
         
-        let ref = Database.database().reference()
-        //Anonymous Firebase Sign-In
-        Auth.auth().signInAnonymously() { (user, error) in
+        fetchDataAndRefreshCells()
+
+    }
+
+    func fetchDataAndRefreshCells() {
+        refHandle = ref?.child("posts").observe(.childAdded, with: { (snapshot) in
+            if let postsDictionary = snapshot.value as? [String : AnyObject] {
+                let post = Post()
+                
+                post.description = postsDictionary["description"] as? String
+                post.timestamp = postsDictionary["timestamp"] as? String
+                post.downloadUrl = postsDictionary["downloadUrl"] as? String
+                
+                guard let url = URL(string: post.downloadUrl!) else { return }
+                URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                    if error != nil {
+                        print(error)
+                        return
+                    }
+                    
+                    DispatchQueue.main.async {
+                        post.image = UIImage(data: data!)
+                        self.posts.insert(post, at: 0)
+                        self.tableView.reloadData()
+                    }
+                    
+                }).resume()
+                
+//                self.posts.append(post)
+//
+//                DispatchQueue.main.async {
+//                    self.tableView.reloadData()
+//                }
+            }
             
-        }
-        
-        //Firebase Live Observer
-        ref.child("posts").observe(.childAdded) { (snapshot) in
-            self.loadCellData()
-        }
-        
-        
-        
+        })
     }
     
-    fileprivate func loadCellData() {
-        let ref = Database.database().reference()
-        ref.child("posts").queryOrdered(byChild: "timestamp").observeSingleEvent(of: .value) { (snapshot) in
-            
-            if let postsDictionary = snapshot.value as? [String : AnyObject] {
-                for post in postsDictionary {
-                    
-                    if let postDictionary = post.value as? [String : AnyObject] {
-                        let post = Post()
-                        post.description = postDictionary["description"] as? String
-                        post.timestamp = postDictionary["timestamp"] as? String
-                        post.downloadUrl = postDictionary["downloadUrl"] as? String
-                        
-                        guard let url = URL(string: post.downloadUrl!) else { return }
-                        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-                            if error != nil {
-                                print(error)
-                                return
-                            }
-                           
-                            DispatchQueue.main.async {
-                                post.image = UIImage(data: data!)
-                            }
-                        }) .resume()
- 
-                        self.posts.insert(post, at: 0)
-                        //self.posts.append(post)
-                    }
-                    self.tableView.reloadData()
-                }
-                
-            }
-
-        }
-    }
+//    fileprivate func loadCellData() {
+//        let ref = Database.database().reference()
+//        ref.child("posts").queryOrdered(byChild: "timestamp").observeSingleEvent(of: .value) { (snapshot) in
+//
+//            if let postsDictionary = snapshot.value as? [String : AnyObject] {
+//                print(postsDictionary)
+////                for post in postsDictionary {
+////                    print(post)
+////                    if let postDictionary = post.value as? [String : AnyObject] {
+////                        let post = Post()
+////                        post.description = postDictionary["description"] as? String
+////                        post.timestamp = postDictionary["timestamp"] as? String
+////                        post.downloadUrl = postDictionary["downloadUrl"] as? String
+//
+////                        guard let url = URL(string: post.downloadUrl!) else { return }
+////                        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+////                            if error != nil {
+////                                print(error)
+////                                return
+////                            }
+////
+////                            DispatchQueue.main.async {
+////                                post.image = UIImage(data: data!)
+////                                self.posts.insert(post, at: 0)
+////                                self.tableView.reloadData()
+////                            }
+////
+////                        }).resume()
+//
+//                   //}
+//
+//                //}
+//                //self.tableView.reloadData()
+//            }
+//
+//        }
+//    }
 
 
 }
